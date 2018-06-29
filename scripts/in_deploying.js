22866cda6bcbc26b5626419e462aae204353b9e9
KID the Euforia <kid0725+github@gmail.com>
2018-06-29 19:18:55 +0900

    notify in deploying
    Signed-off-by: KID the Euforia <kid0725+github@gmail.com>

---
+++ scripts/in_deploying.js
@@ -0,0 +1,44 @@
+"use strict"
+const slackAPI = require('slackbotapi');
+const slackAPIToken = process.env.HUBOT_SLACK_TOKEN;
+
+function initSlackAPI(token) {
+  if (token === undefined) {
+    throw new Error(`HUBOT_SLACK_TOKEN cannot be empty! value: undefined`);
+  }
+  return new slackAPI({
+    'token': token,
+    'logging': false,
+    'autoReconnect': true
+  });
+}
+
+module.exports = (robot) => {
+  let slack;
+  try {
+    slack = initSlackAPI(slackAPIToken);
+  } catch (e) {
+    robot.logger.error(e.toString());
+  }
+
+  function postMessageWithSlack(message, channel, userName, icon) {
+    return new Promise((resolve, reject) => {
+      slack.reqAPI("chat.postMessage", {
+        channel: channel,
+        text: message,
+        username: userName,
+        link_names: 0,
+        pretty: 1,
+        icon_emoji: icon
+      }, (res) => {
+        if(!res.ok) {
+          robot.logger.error(`something occured with slack api. ${res.error}`);
+          reject(new Error(`something occured with slack api. ${res.error}`));
+        }
+        resolve();
+      });
+    });
+  }
+
+  postMessageWithSlack('new revision is deployed!', 'C1N3XHZ0U', 'hubot-pakumori', ':pakumori:');
+};
