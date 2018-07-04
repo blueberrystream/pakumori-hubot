52297534bf8d8521055dfa82f86ea1e75f5468c6
KID the Euforia <kid0725+github@gmail.com>
2018-07-04 21:26:33 +0900

    logging postgres client #14
    Signed-off-by: KID the Euforia <kid0725+github@gmail.com>

--- scripts/store.js
+++ scripts/store.js
@@ -4,17 +4,16 @@ const databaseURL = process.env.DATABASE_URL;
 
 module.exports = (robot) => {
   robot.hear(/https:\/\/(youtu\.be|www\.youtube\.com)\//, (res) => {
-    robot.logger.info(res.message);
-    robot.logger.info(databaseURL);
     if (res.room !== 'C8B0740R1') return;
 
     const client = new Client({
-      connectionString: databaseURL,
+      connectionString: databaseURL
     });
-    const query = 'INSERT INTO "logs" ("username", "raw_text", "ts") VALUES ("$1", "$2", "$3")';
+    const query = 'INSERT INTO "logs" ("username", "raw_text", "ts") VALUES ($1, $2, $3)';
     const values = [res.user.name, res.rawText, res.id];
 
     client.connect();
+    robot.logger.info(client);
     client.query(query, values)
       .then(res => robot.logger.info(res))
       .catch(e => robot.logger.error(e.stack));
